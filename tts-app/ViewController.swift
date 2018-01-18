853341b51647b244be08e94db3de6de961335b2d
takami228 <m.takami228@gmail.com>
2018-01-18 21:24:34 +0900

    Initial Commit

---
+++ tts-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tts-app
+//
+//  Created by takami228 on 2018/01/18.
+//  Copyright © 2018年 takami228. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
