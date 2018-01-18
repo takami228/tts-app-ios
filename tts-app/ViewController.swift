6e59550b0f241eb2d51ae0899a2cb4954cf4ba8d
takami228 <m.takami228@gmail.com>
2018-01-18 23:11:30 +0900

    Add feature for change language

--- tts-app/ViewController.swift
+++ tts-app/ViewController.swift
@@ -10,20 +10,40 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    @IBOutlet var TextLabel: UILabel!
-    @IBOutlet var Button: UIButton!
-    @IBOutlet var TextField: UITextField!
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var button: UIButton!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var modeController: UISegmentedControl!
+    var languageType: String!
+
+    @IBAction func SetLanguageType(_ sender: UISegmentedControl) {
+        if(sender.selectedSegmentIndex == 0){
+            textLabel.text = "Text To Speech Sample App"
+            button.setTitle("Speech", for: .normal)
+            languageType = "en-US"
+            textField.placeholder = "Hello!"
+            textField.keyboardType = UIKeyboardType.alphabet
+        } else {
+            textLabel.text = "テキスト読み上げサンプルアプリ"
+            button.setTitle("読み上げる", for: .normal)
+            languageType = "ja-JP"
+            textField.placeholder = "こんにちは!"
+            textField.keyboardType = UIKeyboardType.default
+        }
+    }
     
     @IBAction func TouchEvent(_ sender: Any) {
         let talker = AVSpeechSynthesizer()
-        let utterance = AVSpeechUtterance(string: TextField.text!)
-        utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")
+        let utterance = AVSpeechUtterance(string: textField.text!)
+        utterance.voice = AVSpeechSynthesisVoice(language: languageType)
         talker.speak(utterance)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        TextField.keyboardType = UIKeyboardType.default
+        textField.keyboardType = UIKeyboardType.alphabet
+        modeController.selectedSegmentIndex = 0
+        languageType = "en-US";
     }
 
     override func didReceiveMemoryWarning() {
